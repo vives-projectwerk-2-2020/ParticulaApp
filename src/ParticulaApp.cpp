efc5df56df07ce9bb61aba6fa64a37a23b48f660
Martijn Everaert <martijn.everaert@gmail.com>
2020-05-08 13:54:42 +0200

    added basic functionality

---
+++ src/ParticulaApp.cpp
@@ -0,0 +1,12 @@
+#include "ParticulaApp.h"
+
+namespace Particula{
+    class ParticulaApp{
+        ParticulaApp::ParticulaApp(){}
+        void ParticulaApp::partSensorSleep(){}
+        void ParticulaApp::partSensorWake(){}
+        void ParticulaApp::tphSensorSleep(){}
+        void ParticulaApp::tphSensorWake(){}
+
+    };
+};
---
+++ src/ParticulaApp.h
@@ -0,0 +1,16 @@
+#pragma once
+
+namespace Particula
+{
+    class ParticulaApp{
+        public:
+            ParticulaApp();
+        public:
+            void partSensorSleep();
+            void partSensorWake();
+            void tphSensorSleep();
+            void tphSensorWake();
+
+
+    };
+};
